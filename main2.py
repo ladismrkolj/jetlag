36eb5cfe761ad7c182ffde6d5a69bf438c00c6ce
Ladi <ladismrkolj@gmail.com>
2025-06-01 19:29:37 +0200

    initial commit

---
+++ main2.py
@@ -0,0 +1,15 @@
+from datetime import datetime, time
+
+def main():
+    tz1_sleep_start = time(22, 0, 0)
+    tz1_sleep_stop = time(7, 0, 0)
+
+    tz2_sleep_start = time(22, 0, 0)
+    tz2_sleep_stop = time(7, 0, 0)
+
+    travel_start = datetime(2025, 6, 1, 14, 30, 45)  
+    travel_stop = datetime(2025, 6, 2, 14, 30, 45)  
+
+
+if __name__ == "__main__":
+    main()
